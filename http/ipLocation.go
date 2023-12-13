214efc1fd536c38bd4e48a9cdb482ccfd79efbb2
爱菲斯尔 <yuyesq@gmail.com>
2023-12-13 23:48:31 +0800

    添加错误处理

--- http/ipLocation.go
+++ http/ipLocation.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/ifeisier/go-utils/text"
 	"strings"
 )
@@ -13,27 +14,49 @@ type IPLocation struct {
 	Latitude  float64 // 纬度
 }
 
-func GetIPLocation(ip string) IPLocation {
-	_, body, _ := GetRequest("http://whois.pconline.com.cn/ipJson.jsp?ip=" + ip + "&json=true")
-	utf8, _ := text.GbkToUtf8(body)
+func GetIPLocation(ip string) (l *IPLocation, e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			l = nil
+			e = fmt.Errorf("未知错误")
+		}
+	}()
+
+	_, body, err := GetRequest("http://whois.pconline.com.cn/ipJson.jsp?ip=" + ip + "&json=true")
+	if err != nil {
+		return nil, err
+	}
+	utf8, err := text.GbkToUtf8(body)
+	if err != nil {
+		return nil, err
+	}
 
 	var bodyMap map[string]interface{}
-	_ = json.Unmarshal(utf8, &bodyMap)
+	err = json.Unmarshal(utf8, &bodyMap)
+	if err != nil {
+		return nil, err
+	}
 
-	_, body, _ = GetRequest("https://api.map.baidu.com/geocoder?address=" + bodyMap["city"].(string) + "&output=json")
-	_ = json.Unmarshal(body, &bodyMap)
+	_, body, err = GetRequest("https://api.map.baidu.com/geocoder?address=" + bodyMap["city"].(string) + "&output=json")
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &bodyMap)
+	if err != nil {
+		return nil, err
+	}
 
-	location := IPLocation{}
+	location := &IPLocation{}
 	location.IP = bodyMap["ip"].(string)
 	location.Addr = strings.Replace(bodyMap["addr"].(string), " ", "", -1)
 
 	if location.Addr == "局域网" || location.Addr == "本机地址" {
-		return location
+		return location, nil
 	}
 
 	lt := bodyMap["result"].(map[string]interface{})["location"].(map[string]interface{})
 	location.Longitude = lt["lng"].(float64)
 	location.Latitude = lt["lat"].(float64)
 
-	return location
+	return location, nil
 }
