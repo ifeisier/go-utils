0b9bc88b05c5f1d043e44a1f6fcfb6111703e62b
爱菲斯尔 <yuyesq@gmail.com>
2023-12-16 23:33:12 +0800

    更新 mysql.go

--- mysql_config/mysql.go
+++ mysql_config/mysql.go
@@ -2,22 +2,46 @@ package mysql_config
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
 type Config struct {
-	Dsn             string `yaml:"dsn"`
-	MaxOpenConns    int    `yaml:"maxOpenConns"`
-	ConnMaxIdleTime int64  `yaml:"connMaxIdleTime"`
+	Dsn             string `yaml:"dsn" json:"dsn"`
+	MaxOpenConn     int    `yaml:"maxOpenConn" json:"maxOpenConn"`
+	ConnMaxIdleTime int64  `yaml:"connMaxIdleTime" json:"connMaxIdleTime"`
 }
 
-func CreateDB(config *Config) (db *sql.DB, err error) {
-	dsn := config.Dsn
-	db, err = sql.Open("mysql", dsn)
+// DefaultMysqlDB 获取空的 MySQL 连接
+func DefaultMysqlDB() **sql.DB {
+	ptrDB := &sql.DB{}
+	return &ptrDB
+}
+
+// CreateDB 创建 MySQL 的数据库连接
+//
+// config 是 MySQL 连接的配置
+//
+// db 是一个二级指针, 通过这个二级指针返回新的 MySQL 连接实例,
+// 如果给定的 db 是已经连接的, 当创建新的连接实例后, 会关闭旧连接实例.
+func CreateDB(config *Config, db **sql.DB) (err error) {
+	newDB, err := sql.Open("mysql", config.Dsn)
 	if err != nil {
-		return
+		return err
 	}
-	db.SetMaxOpenConns(config.MaxOpenConns)
-	db.SetConnMaxIdleTime(time.Duration(config.ConnMaxIdleTime))
+	newDB.SetMaxOpenConns(config.MaxOpenConn)
+	newDB.SetConnMaxIdleTime(time.Duration(config.ConnMaxIdleTime))
+
+	if db == nil {
+		return errors.New("db 不能是 nil")
+	}
+
+	if *db != (&sql.DB{}) {
+		defer func(oldDB *sql.DB) {
+			err = oldDB.Close()
+		}(*db)
+	}
+	*db = newDB
+
 	return
 }
